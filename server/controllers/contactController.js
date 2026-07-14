b2149de23b4ba85d1c74623cdca4d5efbde6fd57
JohnnieKay12 <142480261+JohnnieKay12@users.noreply.github.com>
2026-07-14 16:32:47 +0100

    Fix the transporter

--- server/controllers/contactController.js
+++ server/controllers/contactController.js
@@ -12,16 +12,16 @@ exports.sendContactMessage = async (req, res) => {
     }
 
     const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 587,
+      host: process.env.EMAIL_HOST,
+      port: Number(process.env.EMAIL_PORT),
       secure: false,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
       },
-      tls: {
-        rejectUnauthorized: false,
-      },
+      connectionTimeout: 10000,
+      greetingTimeout: 10000,
+      socketTimeout: 10000,
     });
 
     await transporter.sendMail({
